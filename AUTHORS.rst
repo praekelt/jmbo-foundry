4641ed081e703be3758f1e5649b0dba847540c65
Shaun Sephton <shaunsephton@gmail.com>
2011-09-21 11:34:02 +0200

    first commit

---
+++ AUTHORS.rst
@@ -0,0 +1,9 @@
+Authors
+=======
+
+Praekelt Foundation
+-------------------
+
+* Shaun Sephton
+* Hedley Roos
+
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jmbo-generic',
+    version='0.0.1',
+    description='Jmbo generic behaviour/templates app.',
+    long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
+    author='Praekelt Foundation',
+    author_email='dev@praekelt.com',
+    license='BSD',
+    url='http://github.com/praekelt/jmbo-generic',
+    packages = find_packages(),
+    install_requires = [
+        'django-preferences',
+    ],
+    include_package_data=True,
+    tests_require=[
+        'django-setuptest',
+    ],
+    test_suite="setuptest.SetupTestSuite",
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    zip_safe=False,
+)
