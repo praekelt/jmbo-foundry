78f6936b85a84be2c5a31a07c7e34d5665cf75bd
Shaun Sephton <shaunsephton@gmail.com>
2011-09-21 14:48:39 +0200

    both menu and navbar prefs. is_active lookup

--- generic/models.py
+++ generic/models.py
@@ -1,10 +1,12 @@
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse, Resolver404
 from django.db import models
 
 from preferences.models import Preferences
 
+from snippetscream import resolve_to_name
 
-class FooterMenuLink(models.Model):
+
+class Link(models.Model):
     title = models.CharField(
         max_length=256,
         help_text='A short descriptive title.',
@@ -21,36 +23,63 @@ class FooterMenuLink(models.Model):
         blank=True,
         null=True,
     )
-        
-    class Meta():
-        ordering = ('footermenulinkposition__position',)
 
     def get_absolute_url(self):
+        """
+        Returns url to which link should redirect based on a reversed view name or otherwise explicitly provided url.
+        """
         if self.view_name:
             return reverse(self.view_name)
         else:
             return self.url
 
+    def is_active(self, request):
+        """
+        Determines wheteher or not the link can be consider active based on the request path.
+        True if the request path can be resolved to the same view name as is contained in view_name field.
+        Otherwise True if request path starts with url as contained in url field (needs some work).
+        """
+        try:
+            pattern_name = resolve_to_name(request.path_info)
+        except Resolver404:
+            pattern_name = None
+
+        active = False
+        if pattern_name:
+            active = pattern_name == self.view_name
+        if not active and self.url:
+            active = request.path_info.startswith(self.url)
+        return active
+
     def __unicode__(self):
         return self.title
 
 
-class FooterMenuPreferences(Preferences):
+class MenuPreferences(Preferences):
     __module__ = 'preferences.models'
-    links = models.ManyToManyField(FooterMenuLink, through='generic.FooterMenuLinkPosition')
+    links = models.ManyToManyField(Link, through='generic.MenuLinkPosition')
 
 
-class FooterMenuLinkPosition(models.Model):
-    preferences = models.ForeignKey(FooterMenuPreferences)
-    link = models.ForeignKey(FooterMenuLink)
+class NavbarPreferences(Preferences):
+    __module__ = 'preferences.models'
+    links = models.ManyToManyField(Link, through='generic.NavbarLinkPosition')
+
+
+class LinkPosition(models.Model):
+    link = models.ForeignKey(Link)
     position = models.IntegerField()
 
     class Meta():
+        abstract = True
         ordering = ('position',)
 
     def __unicode__(self):
         return "Link titled %s in position %s." % (self.link.title, self.position)
 
 
-    
+class MenuLinkPosition(LinkPosition):
+    preferences = models.ForeignKey(MenuPreferences)
 
+
+class NavbarLinkPosition(LinkPosition):
+    preferences = models.ForeignKey(NavbarPreferences)
