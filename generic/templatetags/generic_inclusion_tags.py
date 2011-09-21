8bca7a8032a048e4acf7281ece03daf22dc3d9c2
Shaun Sephton <shaunsephton@gmail.com>
2011-09-21 14:47:49 +0200

    added navbar tag, renamed menu, explicit ordering.

--- generic/templatetags/generic_inclusion_tags.py
+++ generic/templatetags/generic_inclusion_tags.py
@@ -5,9 +5,27 @@ from preferences import preferences
 register = template.Library()
 
 
-@register.inclusion_tag('generic/inclusion_tags/footer_menu.html')
-def footer_menu():
-    footer_menu_preferences = preferences.FooterMenuPreferences
+@register.inclusion_tag('generic/inclusion_tags/menu.html')
+def menu():
+    menu_preferences = preferences.MenuPreferences
     return {
-        'object_list': footer_menu_preferences.links.all()
+        'object_list': menu_preferences.links.all().order_by('menulinkposition__position')
+    }
+
+
+@register.inclusion_tag('generic/inclusion_tags/navbar.html', takes_context=True)
+def navbar(context):
+    navbar_preferences = preferences.NavbarPreferences
+    request = context['request']
+    object_list = []
+    active_link = None
+    for link in navbar_preferences.links.all().order_by('navbarlinkposition__position'):
+        object_list.append(link)
+        if not active_link:
+            if link.is_active(request):
+                active_link = link
+
+    return {
+        'object_list': object_list,
+        'active_link': active_link,
     }
