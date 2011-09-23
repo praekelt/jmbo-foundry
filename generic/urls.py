e57289d44e4cbacc9ff014d7edefa43908500977
Shaun Sephton <shaunsephton@gmail.com>
2011-09-23 11:14:50 +0200

    include category urls

--- generic/urls.py
+++ generic/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls.defaults import patterns, url
 from django.views.generic import TemplateView
 
-urlpatterns = patterns(
-    'generic.views',
+from generic.views import CategoryObjectDetailView, CategoryObjectListView
+
+urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name="generic/home.html"), \
             name='home'),
+    url(r'^category/(?P<category_slug>[\w-]+)/$', \
+            CategoryObjectListView.as_view(), name='category_object_list'),
+    url(r'^category/(?P<category_slug>[\w-]+)/(?P<slug>[\w-]+)/$', \
+            CategoryObjectDetailView.as_view(), name='category_object_detail'),
 )
