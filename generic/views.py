43c8e9120f0787bcd0eaeecf04648c5bcfb03d81
Shaun Sephton <shaunsephton@gmail.com>
2011-09-27 15:27:23 +0200

    allow for url callable. detail view

--- generic/views.py
+++ generic/views.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib.auth import authenticate, login, get_backends
+from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect 
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
@@ -10,6 +11,22 @@ from generic.forms import JoinForm
 
 from category.models import Category
 from jmbo.models import ModelBase
+
+class CategoryURL(object):
+
+    def __init__(self, category=None):
+        self.category = category
+
+    def __call__(self, obj=None):
+        if self.category and obj:
+            return reverse(
+                'category_object_detail',
+                kwargs={'category_slug': self.category.slug, 'slug': obj.slug}
+            )
+        elif obj:
+            return obj.get_absolute_url()
+        else:
+            return self
 
 def join(request):
     """Surface join form"""
@@ -28,22 +45,39 @@ def join(request):
     return render_to_response('generic/join.html', extra, context_instance=RequestContext(request))
 
 class CategoryObjectDetailView(DetailView):
-    pass
+    model = ModelBase
+    
+    def get_queryset(self):
+        self.category = get_object_or_404(Category, slug__iexact=self.kwargs['category_slug'])
+        return super(CategoryObjectDetailView, self).get_queryset()
 
+    def get_template_names(self):
+        return ['category/%s_detail.html' % self.category.slug, 'category/detail.html'] + super(CategoryObjectDetailView, self).get_template_names()
+    
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(CategoryObjectDetailView, self).get_context_data(**kwargs)
+        context['category'] = self.category
+        context['object'] = context['object'].as_leaf_class()
+        return context
 
 class CategoryObjectListView(ListView):
     paginate_by = 5
 
     def get_queryset(self):
         self.category = get_object_or_404(Category, slug__iexact=self.kwargs['category_slug'])
-        return ModelBase.permitted.filter(categories=self.category).exclude(pin__category=self.category).order_by('-created')
+        return ModelBase.permitted.filter(categories=self.category).exclude(pin__category=self.category)
         
     def get_template_names(self):
         return ['category/%s_list.html' % self.category.slug, 'category/list.html'] + super(CategoryObjectListView, self).get_template_names()
+    
+    def get_url_callable(self, *args, **kwargs):
+        return CategoryURL(category=self.category)
 
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super(CategoryObjectListView, self).get_context_data(**kwargs)
         context['pinned_object_list'] = ModelBase.permitted.filter(pin__category=self.category).order_by('-created')
         context['category'] = self.category
+        context['url_callable'] = self.get_url_callable()
         return context
