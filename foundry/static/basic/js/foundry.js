a62977b684627ce33ea7e9017f92c75856c73ac8
hedleyroos <hedleyroos@gmail.com>
2011-11-16 08:38:17 +0200

    Intercept ajax form submit

--- foundry/static/basic/js/foundry.js
+++ foundry/static/basic/js/foundry.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
-    $('div.paged_results a').live('click', function(e){
+    // Ajaxify tile paging
+    $('div.foundry-enable-ajax div.paged_results a').live('click', function(e){
         e.preventDefault();
         var target = $(this).parents('div.foundry-enable-ajax:first');
         var url = target.attr('original_url');
@@ -20,6 +21,40 @@ $(document).ready(function(){
                     target.html(data);
             }
         );
+    });
+
+    // Intercept tile form submit
+    $('div.foundry-enable-ajax form').live('submit', function(event){
+        event.preventDefault();
+        var target = $(this).parents('div.foundry-enable-ajax:first');
+        var url = $(this).attr('action');
+        if ((!url) || (url == '.'))
+            url = target.attr('original_url');
+        var data = $(this).serialize();
+        $.ajax({
+            url: url,
+            data: data,
+            async: false,
+            type: 'POST',
+            cache: false,                    
+            success: function(data){
+                if (data.indexOf('{') == 0)
+                {
+                    var obj = $.parseJSON(data);
+                    $('#edit-dialog').dialog('close');
+                }
+                else
+                    if (data.search('id="content"') != -1)
+                    {
+                        // Markup that contains fluff. We want only the content.
+                        var el = $('<div>' + data + '</div>');                   
+                        var content = $('div#content', el);
+                        target.html(content.html());
+                    }
+                    else
+                        target.html(data);
+            }
+        });
     });
 
 });
