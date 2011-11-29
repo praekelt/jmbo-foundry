62489e86fbb47ad05816cf4ae71e31b86db3a995
hedleyroos <hedleyroos@gmail.com>
2011-11-29 12:35:40 +0200

    Clean up static resources

--- foundry/templatetags/listing_styles.py
+++ foundry/templatetags/listing_styles.py
@@ -9,7 +9,7 @@ class AbstractBaseStyle(object):
     
     def get_queryset(self):
         queryset = self.listing.content.all()
-        if not queryset.count():
+        if not queryset.exists():
             queryset = models.ModelBase.permitted.all()
             if self.listing.category:
                 queryset = queryset.filter(categories=self.listing.category)
