bd0a5c2a19c645f9f50bc3fc4578002eb739b735
hedleyroos <hedleyroos@gmail.com>
2011-12-22 11:48:14 +0200

    Ensure context is preserved when passing through listing machinery

--- foundry/templatetags/listing_styles.py
+++ foundry/templatetags/listing_styles.py
@@ -23,15 +23,17 @@ class AbstractBaseStyle(object):
         from foundry import views
         return views.CategoryURL(category=self.listing.category)
     
-    def get_context_data(self, *args, **kwargs):
-        context = {}
+    def get_context_data(self, context):
         context['object_list'] = self.get_queryset()
         context['listing'] = self.listing
         context['url_callable'] = self.get_url_callable()
         return context
 
     def render(self, context):
-        return render_to_string(self.template_name, self.get_context_data(context))
+        context.push()
+        result = render_to_string(self.template_name, self.get_context_data(context))
+        context.pop()
+        return result
 
 
 class Horizontal(AbstractBaseStyle):
