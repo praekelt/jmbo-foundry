a24ef78ff67a0e2700be428938f34dc5fd61c6de
hedleyroos <hedleyroos@gmail.com>
2012-01-20 12:10:29 +0200

    Handle non-ascii for Ambient Mobile

--- foundry/ambientmobile.py
+++ foundry/ambientmobile.py
@@ -183,7 +183,7 @@ class AmbientSMS(object):
         try:
             req = urllib2.Request(url)
             req.add_header( "Content-type" , "application/xml")
-            data = urllib2.urlopen(req, post).read()
+            data = urllib2.urlopen(req, post.encode('utf-8')).read()
         except urllib2.URLError, v:
             raise AmbientSMSError, v
         return dictFromXml(data)
