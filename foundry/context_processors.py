521db66f6d976684de520c60c9862856b06a682a
hedleyroos <hedleyroos@gmail.com>
2012-01-27 09:00:23 +0200

    Add analytics tags

--- foundry/context_processors.py
+++ foundry/context_processors.py
@@ -1,9 +1,12 @@
 from django.contrib.sites.models import get_current_site
 from django.conf import settings
 
+from preferences import preferences
+
 def foundry(request):
     return {
         'FOUNDRY': settings.FOUNDRY,
         'LAYER_PATH': settings.FOUNDRY['layers'][0] + '/',
-        'CURRENT_SITE': get_current_site(request)
+        'CURRENT_SITE': get_current_site(request),
+        'ANALYTICS_TAGS': preferences.GeneralPreferences.analytics_tags
     }
