6bb87a89ca7673f1cbc7137dcdc8975b8794db3d
hedleyroos <hedleyroos@gmail.com>
2012-01-27 08:37:24 +0200

    Softcode site title in base template

--- foundry/context_processors.py
+++ foundry/context_processors.py
@@ -1,7 +1,9 @@
+from django.contrib.sites.models import get_current_site
 from django.conf import settings
 
 def foundry(request):
     return {
         'FOUNDRY': settings.FOUNDRY,
-        'LAYER_PATH': settings.FOUNDRY['layers'][0] + '/'
+        'LAYER_PATH': settings.FOUNDRY['layers'][0] + '/',
+        'CURRENT_SITE': get_current_site(request)
     }
