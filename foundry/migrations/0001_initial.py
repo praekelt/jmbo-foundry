63c15448d60a6550dcaa56b9aa86e560a2c38a3b
hedleyroos <hedleyroos@gmail.com>
2012-05-01 11:39:50 +0200

    Setup South migration chain.

--- foundry/migrations/0001_initial.py
+++ foundry/migrations/0001_initial.py
@@ -6,6 +6,11 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ("jmbo", "0001_initial"),
+        ("friends", "0001_initial"),
+    )
+
     def forwards(self, orm):
         
         # Adding model 'Link'
