1796a61a6d5e0a41e0df0b84fff1222694ed372c
hedleyroos <hedleyroos@gmail.com>
2012-06-22 16:11:16 +0200

    Correct imports

--- demo/foundrydemo/settings_qa_web_site.py
+++ demo/foundrydemo/settings_qa_web_site.py
@@ -1,6 +1,6 @@
 from foundry import settings as foundry_settings
 
-from foundrydemo.settings_live_base import *
+from foundrydemo.settings_qa_base import *
 
 
 FOUNDRY['layers'] = ('web', 'basic',)
