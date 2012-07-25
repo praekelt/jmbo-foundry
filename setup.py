d74a064cc438fe03444b274458f6c3cfc0c8fc0d
Euan Jonker <euan@unomena.com>
2012-07-25 23:30:35 +0200

    Upped the jmbo-friends version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jmbo-foundry',
-    version='0.1.34',
+    version='0.1.35',
     description='Jmbo foundry behaviour/templates app.',
     long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
     author='Praekelt Foundation',
@@ -43,7 +43,7 @@ setup(
                         'http://github.com/praekelt/jmbo-post/tarball/master#egg=jmbo-post',
                         'http://github.com/praekelt/jmbo-show/tarball/master#egg=jmbo-show',
                         'http://github.com/praekelt/jmbo-social/tarball/master#egg=jmbo-social',
-                        'http://github.com/unomena/jmbo-friends/tarball/0.0.13#egg=jmbo-friends-0.0.13',
+                        'http://github.com/unomena/jmbo-friends/tarball/0.0.14#egg=jmbo-friends-0.0.14',
                         'http://github.com/unomena/jmbo-activity/tarball/0.0.6#egg=jmbo-activity-0.0.6',
                         ],
     install_requires = [
@@ -85,7 +85,7 @@ setup(
         'BeautifulSoup',
         'django_compressor',
         'jmbo_analytics',
-        'jmbo-friends==0.0.13',
+        'jmbo-friends==0.0.14',
         'jmbo-activity==0.0.6',
         'jellyfish'
     ],
