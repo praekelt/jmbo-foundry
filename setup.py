b6aa50accabf5bbf13519e45be27ac7afb7f20a3
Euan Jonker <euan@unomena.com>
2012-10-18 19:57:54 +0200

    Fixed the jmbo-friends version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jmbo-foundry',
-    version='0.5.21.unomena',
+    version='0.5.22.unomena',
     description='Jmbo foundry behaviour/templates app.',
     long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
     author='Praekelt Foundation',
@@ -85,7 +85,7 @@ setup(
         'BeautifulSoup',
         'django_compressor',
         'jmbo_analytics',
-        'jmbo-friends==0.1.3.unomena',
+        'jmbo-friends==0.1.4.unomena',
         'jmbo-activity==0.0.8',
         'jellyfish'
     ],
