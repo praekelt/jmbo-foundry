34ade431dafa4d45a383f38b82dc438bb070ea9f
Rizmari Versfeld <rizziepit@gmail.com>
2013-03-11 16:08:18 +0200

    authenticate before login in join form to get user_logged_in signal

--- foundry/views.py
+++ foundry/views.py
@@ -81,8 +81,7 @@ def join(request):
         form = JoinForm(request.POST, request.FILES, show_age_gateway=show_age_gateway, age_gateway_passed=age_gateway_passed, initial=initial) 
         if form.is_valid():
             member = form.save()
-            backend = get_backends()[0]
-            member.backend = "%s.%s" % (backend.__module__, backend.__class__.__name__)
+            member = authenticate(username=member.username, password=form.cleaned_data['password1'])
             login(request, member)            
             response = HttpResponseRedirect(reverse('home'))
             msg = _("You have successfully signed up to the site.")
