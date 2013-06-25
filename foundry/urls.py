06c84cc391d78b0771a51fb9173d638a9253254d
hedleyroos <hedleyroos@gmail.com>
2013-06-25 09:07:49 +0200

    Really remove sitemap

--- foundry/urls.py
+++ foundry/urls.py
@@ -41,7 +41,6 @@ urlpatterns = patterns('',
     ),
 
     (r'^favicon\.ico$', 'django.views.generic.simple.redirect_to', {'url': '/static/images/favicon.ico'}),
-    (r'^', include('jmbo_sitemap.urls')),
     (r'^downloads/', include('downloads.urls')),
     (r'^friends/', include('friends.urls')),
     (r'^gallery/', include('gallery.urls')),
